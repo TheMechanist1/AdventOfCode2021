60fad2bd7d9a87054a0348c0f959c71ac8618670
Mechanist <dminer625@gmail.com>
2021-12-01 23:44:00 -0600

    Day 1 Part 2

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -3,10 +3,11 @@ puzzleinput = open(inputpath,'r')
 
 puzzlelist = []
 
-increase = 0
-decrease = 0
-uhoh = 0
+poneincrease = 0
+ponedecrease = 0
+poneuhoh = 0
 
+#Part 1
 for pi in puzzleinput.readlines():
     puzzlelist.append(int(pi.replace("\n", "")))
 
@@ -15,11 +16,29 @@ print(puzzlelist)
 for x in range(len(puzzlelist)):
     if(x > 0):
         if(puzzlelist[x] > puzzlelist[x - 1]):
-            increase += 1
+            poneincrease += 1
         elif(puzzlelist[x] < puzzlelist[x - 1]):
-            decrease += 1
+            ponedecrease += 1
         else:
-            uhoh += 1
+            poneuhoh += 1
 
-print(str(increase) + "\n" + str(decrease) + "\n" + str(uhoh))
-            
+print(str(poneincrease) + "\n" + str(ponedecrease) + "\n" + str(poneuhoh))
+
+ptwoincrease = 0
+ptwodecrease = 0
+ptwouhoh = 0
+
+#Part 2
+for y in range(len(puzzlelist)):
+    if(y > 0 and y < len(puzzlelist) - 2):
+        thissection = puzzlelist[y] + puzzlelist[y + 1] + puzzlelist[y + 2]
+        prevsection = puzzlelist[y - 1] + puzzlelist[y] + puzzlelist[y + 1]
+
+        if(thissection > prevsection):
+            ptwoincrease += 1
+        elif(thissection < prevsection):
+            ptwodecrease += 1
+        else:
+            ptwouhoh += 1
+
+print(str(ptwoincrease) + "\n" + str(ptwodecrease) + "\n" + str(ptwouhoh))
